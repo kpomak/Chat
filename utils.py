435a866c02bfd9bf19e089953a1b295619780de2
Roman Ryzhkov <93090129+kpomak@users.noreply.github.com>
2023-03-01 18:55:01 +0300

    Tests (#2)
    * server tests
    
    * client test
    
    * tests
    
    * class based client-server
    
    * classbased tests

--- utils.py
+++ utils.py
@@ -4,22 +4,24 @@ import time
 from config import MAX_PACKAGE_LENGTH, ENCODING, ERRORS
 
 
-def get_message(sock):
-    response = sock.recv(MAX_PACKAGE_LENGTH).decode(ENCODING)
-    return json.loads(response)
+class Chat:
+    @staticmethod
+    def get_message(sock):
+        response = sock.recv(MAX_PACKAGE_LENGTH).decode(ENCODING)
+        return json.loads(response)
 
+    @staticmethod
+    def send_message(sock, message):
+        message = json.dumps(message).encode(ENCODING)
+        sock.send(message)
 
-def send_message(sock, message):
-    message = json.dumps(message).encode(ENCODING)
-    sock.send(message)
+    @staticmethod
+    def template_message(action="msg", **kwargs):
+        message = {"action": action, "time": time.time()}
+        for key, value in kwargs.items():
+            message[key] = value
+        return message
 
-
-def template_message(action="msg", **kwargs):
-    message = {"action": action, "time": time.time()}
-    for key, value in kwargs.items():
-        message[key] = value
-    return message
-
-
-def get_error():
-    return random.choice(ERRORS)
+    @staticmethod
+    def get_error():
+        return random.choice(ERRORS)
